26cac9ab8c9e2bcd07a42f3f198f20916eb59729
Mark T. Holder <mtholder@gmail.com>
2013-09-25 11:25:11 -0500

    printing microsat type

--- flow-control/microsat.R
+++ flow-control/microsat.R
@@ -7,21 +7,20 @@ data <- "ACGTAACGTCGCGCGCTAACGCTAGCTAGCTAGCGGCTATATATATATAAGCGATGCTGCATGCTAGCGCT
 #   38, 12
 seq.list <- strsplit(data,'')[[1]];
 n <- length(seq.list)
-running.count <- 0;
+
 to.print.indices <- c();
 to.print.lengths <- c();
-for (i in 3 : n) {
-    if (seq.list[i] == seq.list[i-2]) {
+i <- 3
+while (i <= n) {
+    running.count <- 0;
+    while (seq.list[i] == seq.list[i-2]) {
         running.count <- running.count + 1;
-    } else {
-        if (running.count > 2) {
-            to.print.indices = c(to.print.indices, i - running.count - 2);
-            to.print.lengths = c(to.print.lengths, running.count + 2);
-        }
-        running.count <- 0;
+        i <- 1 + i;
     }
-}
-
-for (j in 1:length(to.print.indices)) {
-    cat(to.print.indices[j], " ", to.print.lengths[j], "\n")
+    if (running.count > 2) {
+        f <- seq.list[i - running.count - 2];
+        s <- seq.list[i - running.count - 1];
+        cat(i - running.count - 2, " ", running.count + 2, " ", paste(f, s, sep=""), "\n");
+    }
+    i <- 1 + i;
 }
