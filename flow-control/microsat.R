6da0825844fb28963bb62076b690553cea60904f
Mark T. Holder <mtholder@gmail.com>
2013-09-25 11:18:13 -0500

    with for loops

--- flow-control/microsat.R
+++ flow-control/microsat.R
@@ -4,9 +4,24 @@ data <- "ACGTAACGTCGCGCGCTAACGCTAGCTAGCTAGCGGCTATATATATATAAGCGATGCTGCATGCTAGCGCT
 #                 *******                     ************                        
 # desired output:
 #   10, 7
-#   38, 
-seq.list = strsplit(data,'')[[1]];
-n = length(seq.list)
-for (i in 1 : n) {
-    cat(seq.list[[i]], "\n")
+#   38, 12
+seq.list <- strsplit(data,'')[[1]];
+n <- length(seq.list)
+running.count <- 0;
+to.print.indices <- c();
+to.print.lengths <- c();
+for (i in 3 : n) {
+    if (seq.list[i] == seq.list[i-2]) {
+        running.count <- running.count + 1;
+    } else {
+        if (running.count > 2) {
+            to.print.indices = c(to.print.indices, i - running.count - 2);
+            to.print.lengths = c(to.print.lengths, running.count + 2);
+        }
+        running.count <- 0;
+    }
+}
+
+for (j in 1:length(to.print.indices)) {
+    cat(to.print.indices[j], " ", to.print.lengths[j], "\n")
 }
